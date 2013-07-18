96b6476d5797d31b977940100e54dd8a72d3af05
deveshj4 <deveshj4@gmail.com>
2013-07-18 19:36:46 +0000

    Added node_modules. Modified web.js to view changes on webpage in real time.

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fs = require('fs');
   var text = fs.readFileSync("index.html", "utf-8");
   response.send(text);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080; //5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
